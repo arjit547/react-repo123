d2269a5e0b6d1748491425a76f1e8323085a5b8c
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-12-18 19:36:43 +0530

    Update sonar_analysis.sh

--- sonar_analysis.sh
+++ sonar_analysis.sh
@@ -4,16 +4,14 @@ SONAR_ISSUES=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/me
 
 if [ "$SONAR_ISSUES" -gt 0 ]; then
   echo "SonarQube analysis found $SONAR_ISSUES issues. Invoking Lambda function..."
+  
+  # Get detailed issue information
+  ISSUES_DETAILS=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/issues/search?componentKeys=arjit547_react-repo123&types=BUG&ps=1" | jq -r '.issues[0].message')
 
-  # Escape special characters in the issues details
-  ISSUES_DETAILS=$(echo "$SONAR_ISSUES" | sed 's/"/\\"/g')
-
-  PAYLOAD="{\"exit_status\": 1, \"issues\": \"$ISSUES_DETAILS\"}"
-
-  # Base64 encode the payload
-  ENCODED_PAYLOAD=$(echo -n "$PAYLOAD" | base64)
-
-  aws lambda invoke --function-name sonarlambda --payload "$ENCODED_PAYLOAD" output.txt
+  # Invoke Lambda function with issue details
+  PAYLOAD=$(echo -n "{\"exit_status\": 1, \"issues\": \"$ISSUES_DETAILS\"}" | base64)
+  aws lambda invoke --function-name sonarlambda --payload "$PAYLOAD" output.txt
+  
 else
   echo "SonarQube analysis succeeded. No issues found."
 fi
