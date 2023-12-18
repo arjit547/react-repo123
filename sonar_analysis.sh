45c95bc3312cb76a717c0b5deca004ab565897cd
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-12-18 20:09:53 +0530

    Update sonar_analysis.sh

--- sonar_analysis.sh
+++ sonar_analysis.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
- 
-SONAR_ISSUES=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/measures/component?component=arjit547_react-repo123&metricKeys=bugs" | jq -r '.component.measures[0].value')
- 
+
+# Fetch SonarQube token from AWS CodeBuild environment variable
+SONAR_TOKEN="$SONAR_TOKEN"
+SONAR_PROJECT_KEY="arjit547_react-repo123"
+
+SONAR_ISSUES=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/measures/component?component=${SONAR_PROJECT_KEY}&metricKeys=bugs" | jq -r '.component.measures[0].value')
+
 if [ "$SONAR_ISSUES" -gt 0 ]; then
   echo "SonarQube analysis found $SONAR_ISSUES issues. Invoking Lambda function..."
-  PAYLOAD=$(echo -n "{\"exit_status\": 1}" | base64)
-  INVOKE_OUTPUT=$(aws lambda invoke --function-name sonarlambda --payload "$PAYLOAD" output.txt 2>&1)
-  echo "Lambda function output: $INVOKE_OUTPUT"
+  PAYLOAD=$(echo -n "{\"exit_status\": 1, \"issues\": \"$SONAR_ISSUES\"}" | base64)
+  aws lambda invoke --function-name sonarlambda --payload "$PAYLOAD" output.txt
 else
   echo "SonarQube analysis succeeded. No issues found."
 fi
