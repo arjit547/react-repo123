76015e5366628f708a839f9680d959b1ea0c30fc
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-12-18 19:22:48 +0530

    Update sonar_analysis.sh

--- sonar_analysis.sh
+++ sonar_analysis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
- 
-SONAR_ISSUES=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/measures/component?component=arjit547_react-repo123&metricKeys=bugs" | jq -r '.component.measures[0].value')
- 
-if [ "$SONAR_ISSUES" -gt 0 ]; then
-  echo "SonarQube analysis found $SONAR_ISSUES issues. Invoking Lambda function..."
+
+SONAR_ISSUES=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/issues/search?componentKeys=arjit547_react-repo123&resolved=false" | jq -r '.issues | .[] | .message')
+
+if [ -n "$SONAR_ISSUES" ]; then
+  echo "SonarQube analysis found issues. Invoking Lambda function..."
   PAYLOAD=$(echo -n "{\"exit_status\": 1, \"issues\": \"$SONAR_ISSUES\"}" | base64)
   aws lambda invoke --function-name sonarlambda --payload "$PAYLOAD" output.txt
 else
