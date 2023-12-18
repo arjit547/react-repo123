423aa4fa4ba9c263fc117cebe76a12ec8454cdfd
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-12-18 20:35:37 +0530

    Update sonar_analysis.sh

--- sonar_analysis.sh
+++ sonar_analysis.sh
@@ -7,13 +7,16 @@ SONAR_PROJECT_KEY="arjit547_react-repo123"
 # Fetch SonarQube analysis details
 SONAR_DETAILS=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/qualitygates/project_status?projectKey=${SONAR_PROJECT_KEY}" | jq -r '.projectStatus.status')
 
-# Fetch SonarQube issues count
-SONAR_ISSUES=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/measures/component?component=${SONAR_PROJECT_KEY}&metricKeys=bugs" | jq -r '.component.measures[0].value')
+# Fetch SonarQube issues count for new code
+SONAR_NEW_ISSUES=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/measures/component?component=${SONAR_PROJECT_KEY}&metricKeys=new_bugs" | jq -r '.component.measures[0].value')
+
+# Fetch SonarQube issues count for overall code
+SONAR_OVERALL_ISSUES=$(curl -s -u "${SONAR_TOKEN}": -X GET "https://sonarcloud.io/api/measures/component?component=${SONAR_PROJECT_KEY}&metricKeys=bugs" | jq -r '.component.measures[0].value')
 
 if [ "$SONAR_DETAILS" == "OK" ]; then
   echo "SonarQube analysis passed quality gate. No issues found."
 else
   echo "SonarQube analysis failed quality gate. Invoking Lambda function..."
-  PAYLOAD=$(echo -n "{\"exit_status\": 1, \"quality_gate_status\": \"$SONAR_DETAILS\", \"issues\": \"$SONAR_ISSUES\"}" | base64)
+  PAYLOAD=$(echo -n "{\"exit_status\": 1, \"quality_gate_status\": \"$SONAR_DETAILS\", \"new_issues\": \"$SONAR_NEW_ISSUES\", \"overall_issues\": \"$SONAR_OVERALL_ISSUES\"}" | base64)
   aws lambda invoke --function-name sonarlambda --payload "$PAYLOAD" output.txt
 fi
